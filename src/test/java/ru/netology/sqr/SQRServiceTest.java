4c3614bd09f1e82337855a3867a00325f6c73b8b
AlexandrP88 <alexsandr@rtural.ru>
2021-11-09 11:46:06 +0500

    added tests

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @org.junit.jupiter.api.Test
-    void shouldcalculateNumSquare() {
+    void shouldCalculateNumSquareTwo() {
         SQRService sqrService = new SQRService();
 
         int expected = 3;
@@ -14,4 +14,35 @@ class SQRServiceTest {
         assertEquals(expected, actual);
 
     }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateNumSquareZero() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 0;
+        int actual = sqrService.calculateNumSquare(0, 99);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateNumSquareLowerLimit() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 1;
+        int actual = sqrService.calculateNumSquare(0, 100);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateNumSquareHigherLimit() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 90;
+        int actual = sqrService.calculateNumSquare(0, 9801);
+
+        assertEquals(expected, actual);
+    }
+
 }
